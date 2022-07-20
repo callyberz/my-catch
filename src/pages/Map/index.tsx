7079be392fa5d3ec7c68ec8c6ad8ce23bb32b356
hkclki <hokheil26@gmail.com>
2022-07-21 01:19:35 +0800

    add geocoder search bar

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,17 +1,52 @@
-import Map from 'react-map-gl';
+import Map, { MapboxEvent, Marker, NavigationControl } from 'react-map-gl';
+import Pin from '../../assets/Pin';
+import GeocoderControl from '../../components/GeocoderControl';
+import { MAPBOX_STYLES } from '../../constants/constant';
 import { enviroment } from '../../environment/env';
 
+const geoJsonData = [
+  {
+    longitude: 114.1694,
+    latitude: 22.3193
+  },
+  {
+    longitude: 114.1694,
+    latitude: 22.4193
+  }
+];
+
 export const MapComp = (props: any) => {
+  const { mapBoxToken } = enviroment;
+  const markerOnClick = (event: MapboxEvent<MouseEvent>) => {
+    console.log('clicked');
+  };
+
   return (
     <Map
-      mapboxAccessToken={enviroment.mapBoxToken}
+      mapboxAccessToken={mapBoxToken}
       initialViewState={{
         latitude: 22.3193,
         longitude: 114.1694,
         zoom: 10
       }}
-      style={{ width: 1200, height: 800 }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+      style={{ width: 800, height: 600 }}
+      mapStyle={MAPBOX_STYLES.outdoors}
+    >
+      {geoJsonData.map((item) => (
+        <Marker
+          key={`${item.latitude}-${item.longitude}`}
+          latitude={item.latitude}
+          longitude={item.longitude}
+          anchor="bottom"
+          onClick={markerOnClick}
+        >
+          <Pin size={20} />
+        </Marker>
+      ))}
+
+      <GeocoderControl mapboxAccessToken={mapBoxToken} />
+
+      <NavigationControl />
+    </Map>
   );
 };
