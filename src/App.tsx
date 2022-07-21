a0f912b791ccf66c8f1e8a4097b7e5c0eac277b8
hkclki <hokheil26@gmail.com>
2022-07-21 21:29:06 +0800

    make MapBox full screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { MapComp } from './pages/Map';
 const App = () => {
   return (
     <div className="App">
-      <h1>Here is my catch!</h1>
       <Routes>
         <Route path="/" element={<MapComp />} />
       </Routes>
