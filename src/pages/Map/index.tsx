0ba0374d5da75d92aed55a0a0ea02a5b1189722e
hkclki <hokheil26@gmail.com>
2022-07-21 21:22:37 +0800

    add webpack babel

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -3,6 +3,7 @@ import Pin from '../../assets/Pin';
 import GeocoderControl from '../../components/GeocoderControl';
 import { MAPBOX_STYLES } from '../../constants/constant';
 import { enviroment } from '../../environment/env';
+import './index.less';
 
 const geoJsonData = [
   {
@@ -22,31 +23,34 @@ export const MapComp = (props: any) => {
   };
 
   return (
-    <Map
-      mapboxAccessToken={mapBoxToken}
-      initialViewState={{
-        latitude: 22.3193,
-        longitude: 114.1694,
-        zoom: 10
-      }}
-      style={{ width: 800, height: 600 }}
-      mapStyle={MAPBOX_STYLES.outdoors}
-    >
-      {geoJsonData.map((item) => (
-        <Marker
-          key={`${item.latitude}-${item.longitude}`}
-          latitude={item.latitude}
-          longitude={item.longitude}
-          anchor="bottom"
-          onClick={markerOnClick}
-        >
-          <Pin size={20} />
-        </Marker>
-      ))}
+    <div className="mapcon">
+      <Map
+        mapboxAccessToken={mapBoxToken}
+        initialViewState={{
+          latitude: 22.3193,
+          longitude: 114.1694,
+          zoom: 10
+        }}
+        style={{ width: 500, height: 800 }}
+        // style={{ width: '100%', height: '100%' }}
+        mapStyle={MAPBOX_STYLES.outdoors}
+      >
+        {geoJsonData.map((item) => (
+          <Marker
+            key={`${item.latitude}-${item.longitude}`}
+            latitude={item.latitude}
+            longitude={item.longitude}
+            anchor="bottom"
+            onClick={markerOnClick}
+          >
+            <Pin size={20} />
+          </Marker>
+        ))}
 
-      <GeocoderControl mapboxAccessToken={mapBoxToken} />
+        <GeocoderControl mapboxAccessToken={mapBoxToken} />
 
-      <NavigationControl />
-    </Map>
+        <NavigationControl />
+      </Map>
+    </div>
   );
 };
